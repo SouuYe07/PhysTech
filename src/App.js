75cc9e4b2e5d13bae259f55d266368086156afc4
Iesou Aguila <iesouaguila@gmail.com>
2026-06-04 11:50:25 +0800

    Configured nativewind

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <Text className="text-lg font-bold">Open up App.jswoperiopw to start working on your app!</Text>
+    </>
+  );
+}
+
