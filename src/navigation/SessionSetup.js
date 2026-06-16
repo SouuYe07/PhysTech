1af5568c3352c7da24dbb979680a20b7e59779d4
SouuYe07 <iesouaguila07@gmail.com>
2026-06-16 23:58:54 +0800

    Map functionality

--- src/navigation/SessionSetup.js
+++ src/navigation/SessionSetup.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Map, Camera } from '@maplibre/maplibre-react-native';
 import { Text } from 'react-native';
 
 export default function SessionSetup() {
     return (
-        <Text> Session </Text>
+        <Map mapStyle="https://tiles.openfreemap.org/styles/dark">
+            <Camera 
+                zoomLevel={14}
+                centerCoordinate={[121.0, 14.5]}
+                pitch={45}      // Tilt
+                heading={30}    // Rotation
+                animationDuration={2000} // Transition
+            />
+        </Map>
     )
 }
