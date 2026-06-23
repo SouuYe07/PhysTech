f3dd609a76cba0cf0fc167dda277de2f064dc8f4
SouuYe07 <iesouaguila07@gmail.com>
2026-06-23 20:42:47 +0800

    Finalized Session Type

--- src/components/setup-components/SessionType.js
+++ src/components/setup-components/SessionType.js
@@ -17,8 +17,34 @@ export default function SessionType(){
             <Text className="font-heading text-[#A6A3A3] tracking-[0px]">
                 Session Type
             </Text>
-            <View className="w-full h-16 bg-[#303030] rounded-lg flex-row">
+            <View className="w-full h-16 bg-[#303030] rounded-lg flex-row justify-center gap-x-[6px]">
                 <TouchableOpacity className="w-[48%] items-center justify-center flex-row" onPress={() => setSelectedType("Solo")}>
+                    <LinearGradient
+                        style={{
+                            borderRadius: 5,
+                            height: '90%',
+                            width: '95%', 
+                            overflow: 'hidden',
+                            justifyContent: 'flex-start',
+                            alignItems: 'center',
+                            flexDirection: 'row',
+                            borderWidth: 1,
+                            borderColor: selectedType === "Solo" ? "#FFC710" : "#6A6A6A"
+                        }}
+                        colors={['#141414', '#1E1E1E']}
+                        start={{ x: 0.45, y: 0 }}
+                        end={{ x: 0.55, y: 1 }}
+                    >
+                        <View className="h-10 w-10 bg-[#303030] justify-center items-center rounded-lg ml-3 flex-row">
+                            <Solo color={selectedType == "Group" ? "#FFC710" : "#808080"}/>
+                        </View>
+                        <Text className={`font-heading text-xl ml-2 ${selectedType === "Solo" ? "text-[#FFC710]" : "text-[#808080]"}`}>
+                            Solo
+                        </Text>
+                    </LinearGradient>
+                </TouchableOpacity>
+                <View className="h-full w-[1px] bg-black"/>
+                <TouchableOpacity className="w-[48%] items-center justify-center flex-row" onPress={() => setSelectedType("Group")}>
                     <LinearGradient
                         style={{
                             borderRadius: 5,
@@ -26,17 +52,20 @@ export default function SessionType(){
                             width: '95%', 
                             justifyContent: 'flex-start',
                             alignItems: 'center',
-                            flexDirection: 'row'
+                            flexDirection: 'row',
+                            overflow: 'hidden',
+                            borderWidth: 1,
+                            borderColor: selectedType === "Group" ? "#FFC710" : "#6A6A6A"
                         }}
                         colors={['#141414', '#1E1E1E']}
                         start={{ x: 0.45, y: 0 }}
                         end={{ x: 0.55, y: 1 }}
                     >
                         <View className="h-10 w-10 bg-[#303030] justify-center items-center rounded-lg ml-3 flex-row">
-                            <Solo />
+                            <Group color={selectedType == "Group" ? "#FFC710" : "#808080"}/>
                         </View>
-                        <Text className="font-heading text-white text-xl ml-2">
-                            Solo
+                        <Text className={`font-heading text-xl ml-2 ${selectedType === "Solo" ? "text-[#FFC710]" : "text-[#808080]"}`}>
+                            Group
                         </Text>
                     </LinearGradient>
                 </TouchableOpacity>
