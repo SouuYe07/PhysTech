209e0bf05711dcb27f39923625d513bcdcff6be5
SouuYe07 <iesouaguila07@gmail.com>
2026-06-26 13:15:48 +0800

    Added initial location tracker

--- src/navigation/SessionMap.js
+++ src/navigation/SessionMap.js
@@ -1,6 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { View, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import { Map, Camera } from '@maplibre/maplibre-react-native';
+import { Map, Camera, Marker } from '@maplibre/maplibre-react-native';
+import { useFocusEffect } from '@react-navigation/native';
+
+import * as expoLocation from 'expo-location';
+
+import Location from '../../assets/map-icons/Location.svg';
 
 import { tabBarStyle } from '../components/TabBarStyle.js';
 import SessionSetup from "../components/setup-components/SessionSetup.js";
@@ -9,7 +14,7 @@ import SessionProgress from '../components/SessionProgress';
 
 
 export default function SessionMap({ navigation }){
-    const [sessionStarted, setSessionStarted] = useState(false);
+    const [sessionStarted, setSessionStarted] = useState(true);
     const [config, setConfig] = useState(null);
     const [isPaused, setIsPaused] = useState(false);
     
@@ -26,7 +31,30 @@ export default function SessionMap({ navigation }){
     const [distanceKm, setDistanceKm] = useState(0);
     const [paceMinPerKm, setPaceMinPerKm] = useState(0);
 
-    
+    const [currentLocation, setCurrentLocation] = useState(null);
+
+    const getCurrentLocation = async () => {
+        const { status } = await expoLocation.requestForegroundPermissionsAsync();
+        if (status !== 'granted'){
+            navigation.navigate('Home');
+            return;
+        }
+
+        const location = await expoLocation.getCurrentPositionAsync({
+            accuracy: expoLocation.Accuracy.Balanced
+        });
+        const { latitude, longitude } = location.coords;
+        console.log(`${latitude}, ${longitude}`)
+
+        setCurrentLocation({ latitude, longitude });
+    };
+
+    useFocusEffect(
+        useCallback(() => {
+            getCurrentLocation();
+        }, [])
+    );
+
     useEffect(() => {
         navigation.setOptions({
             tabBarStyle: sessionStarted ? tabBarStyle : {display: 'none'},
@@ -36,12 +64,12 @@ export default function SessionMap({ navigation }){
     );
 
     useEffect(() => {
-    if (sessionStarted || isPaused) return;
-    const timer = setInterval(() => {
-        setElapsedSeconds(s => s + 1);
-    }, 1000);
-    return () => clearInterval(timer);
-}, [sessionStarted, isPaused]);
+        if (sessionStarted || isPaused) return;
+        const timer = setInterval(() => {
+            setElapsedSeconds(s => s + 1);
+        }, 1000);
+        return () => clearInterval(timer);
+    }, [sessionStarted, isPaused]);
 
     return(
         <View className="relative w-full h-full">
@@ -50,13 +78,24 @@ export default function SessionMap({ navigation }){
                 logo={false}
                 attribution={false}
             >
-                <Camera 
-                    zoomLevel={14}
-                    centerCoordinate={[121.0, 14.5]}
-                    pitch={45}      // Tilt
-                    heading={30}    // Rotation
-                    animationDuration={2000} // Transition
-                />
+                {currentLocation && (
+                    <>
+                        <Marker
+                            id="me"
+                            lngLat={[ currentLocation.longitude, currentLocation.latitude ]}
+                            anchor={{ x: 0.5, y:1 }}
+                        >   
+                            <Location />
+                        </Marker>
+                        <Camera 
+                            zoom={16}
+                            center={[currentLocation.longitude, currentLocation.latitude ]}
+                            pitch={45}      // Tilt
+                            bearing={30}    // Rotation
+                            duration={2000} // Transition
+                        />
+                    </>
+                )}
             </Map>
 
             {!sessionStarted && (
